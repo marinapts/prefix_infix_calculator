d2cdeda9514b330bd5bb0098c93d4bacfea2dcc8
marinapts <potsimarina@gmail.com>
2021-01-31 22:34:11 +0000

    Add option for passing arguments or input interaction

--- calculator.py
+++ calculator.py
@@ -1,15 +1,31 @@
-import pytest
+import argparse
 import re
 
 
-def prefix_calc(prefix_input):
-    if not prefix_input: return
+def compute_calculation(input_str):
+    assert bool(re.search('[a-zA-Z]', input_str)) is False
+
+    if ')' in input_str:  # infix notation
+        res = infix_calc(input_str)
+        print('Infix result: ', res)
+    else:  # prefix notation
+        res = prefix_calc(input_str)
+        print('Prefix result:', res)
+
+    return res
+
+def prefix_calc(prefix_str):
+    prefix_arr = prefix_str.split()
+    prefix_arr.reverse()
+
+    if not prefix_arr:
+        return None
 
     operators = set(['+', '-', '*', '/'])
     stack = []
 
-    while prefix_input:
-        curr = prefix_input.pop(0)
+    while prefix_arr:
+        curr = prefix_arr.pop(0)
         if curr not in operators:
             stack.append(curr)
         else:
@@ -21,12 +37,15 @@ def prefix_calc(prefix_input):
     return stack[0]
 
 
-def infix_calc(infix_input):
-    if not infix_input: return
+def infix_calc(infix_str):
+    infix_arr = infix_str.replace(')', '').split()
+
+    if not infix_arr:
+        return None
 
     stack = []
-    while infix_input:
-        curr = infix_input.pop()
+    while infix_arr:
+        curr = infix_arr.pop()
         if curr != '(':
             stack.append(curr)
         else:
@@ -40,15 +59,16 @@ def infix_calc(infix_input):
 
 
 if __name__ == '__main__':
-    input_str = ' + 1 * 2 3'
-    input_str = '( 1 + ( 2 * 3 ) )'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', required=False, help='Expression for prefix or infix calculation')
+    args = parser.parse_args()
+    input_str = args.input
 
-    assert bool(re.search('[a-zA-Z]', input_str)) is False
+    if input_str:  # Input expression was not passed through the command line
+        compute_calculation(input_str)
+    else:  # Allow for user interaction using the input prompt
+        while True:
+            input_str = input('Enter prefix or infix expression: ')
+            if not input_str: break
+            compute_calculation(input_str)
 
-    if ')' in input_str:  # infix notation
-        input_arr = input_str.replace(')', '').split()
-        print(infix_calc(input_arr))
-    else:  # prefix notation
-        input_arr = input_str.split()
-        input_arr.reverse()
-        print(prefix_calc(input_arr))
